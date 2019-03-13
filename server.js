0a1dea0648a9cdfc327bbfb119aea4ef5acc3177
kashyapkbandi <kashyapkbandi@users.noreply.github.com>
2019-03-14 00:59:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs'); 
+
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+// __dirname - stores path to directory
+
+app.use(express.static(__dirname + '/assets'));
+const viewPath = path.join(__dirname,'./templates/views');
+
+console.log(viewPath);
+// tell express to use templates instead of views. 
+
+app.set('views',viewPath);
+
+app.get('',(req,res)=>{
+res.render('index');
+});
+
+
+app.get('/page2',(req,res)=>{
+res.render('page2',{
+  jsPath: __dirname+'/assets/app.js'
+  
+});
+});
+
+app.set('view engine','hbs');
+
+
+
+
+app.listen(port);
